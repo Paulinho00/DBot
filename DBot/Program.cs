8641f94b3a1d44455cbb24ef60485a439bdcda98
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-06-28 13:39:55 +0200

    Bot initialization
    Added bot login functionality and basic log handler

---
+++ DBot/Program.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    /// <summary>
+    /// Bot client
+    /// </summary>
+    private DiscordSocketClient _client;
+
+    public static Task Main(string[] args)
+    {
+       return new Program().MainAsync();
+    }
+
+    /// <summary>
+    /// Async context of application
+    /// </summary>
+    /// <returns></returns>
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        //Assign _client's Log event to Log handler implementation
+        _client.Log += Log;
+
+        //Change token source dependent on your way of storing
+        await _client.LoginAsync(TokenType.Bot, File.ReadAllText("../../../config.txt").Trim());
+        await _client.StartAsync();
+
+        //Block using task until the program is closed
+        await Task.Delay(-1);
+
+    }
+
+    /// <summary>
+    /// Return log messages on console
+    /// </summary>
+    /// <param name="msg">LogMessage object provided by log source</param>
+    /// <returns></returns>
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
