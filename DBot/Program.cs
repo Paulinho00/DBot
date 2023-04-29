923278e46af483b32e401f5d6f66eb52eec4ed50
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2023-04-29 09:56:46 +0200

    Bot connects to discord API

--- DBot/Program.cs
+++ DBot/Program.cs
@@ -1,9 +1,29 @@
 ﻿using DBot;
+using DisCatSharp;
+using DisCatSharp.Enums;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainAsync()
+    {
+        var discord = new DiscordClient(new DiscordConfiguration()
+        {
+            Token = File.ReadAllText("config.txt").Trim(),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.Guilds |
+                      DiscordIntents.GuildMembers |
+                      DiscordIntents.GuildMessageReactions |
+                      DiscordIntents.GuildMessages |
+                      DiscordIntents.GuildVoiceStates |
+                      DiscordIntents.GuildPresences
+        });
+
+        await discord.ConnectAsync();
+        await Task.Delay(-1);
     }
 }
